9aa7eeb20f401e405fe1490f50a7b8d7fb9516e3
tubasaaa <Yuto21.yokudesu@gmail.com>
2021-09-09 16:47:42 +0900

    progress_func fix

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ st.title('Streamlit 超入門')
 st.write('DataFrame')
 
 df = pd.DataFrame({
-  '１列目': [1, 2, 3, 4],
-  '２列目': [10, 20, 30, 40]
+    '１列目': [1, 2, 3, 4],
+    '２列目': [10, 20, 30, 40]
 })
 
 # st.dataframe(df.style.highlight_max(axis=0), width=200, height=200)
@@ -34,8 +34,8 @@ df = pd.DataFrame({
 # """
 
 df2 = pd.DataFrame(
-  np.random.rand(20,3),
-  columns = ['a', 'b', 'c']
+    np.random.rand(20, 3),
+    columns=['a', 'b', 'c']
 )
 
 # df2
@@ -45,8 +45,8 @@ df2 = pd.DataFrame(
 st.bar_chart(df2)
 
 df3 = pd.DataFrame(
-  np.random.rand(100,2)/[50,50] + [35.69, 139.70],
-  columns = ['lat', 'lon']
+    np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
+    columns=['lat', 'lon']
 )
 
 st.map(df3)
@@ -54,14 +54,14 @@ st.map(df3)
 st.write('Display Image')
 
 if st.checkbox('show Image'):
-  img = Image.open('sample.jpg')
-  st.image(img, caption='sample', use_column_width=True)
+    img = Image.open('sample.jpg')
+    st.image(img, caption='sample', use_column_width=True)
 
 ###
 
 option = st.selectbox(
-  'あなたが好きな数字を教えて下さい。',
-  list(range(1,11))
+    'あなたが好きな数字を教えて下さい。',
+    list(range(1, 11))
 )
 'あなたの好きな数字は、', option, 'です'
 
@@ -79,7 +79,7 @@ condision = st.slider('あなたの今の調子は？', 0, 100, 50)
 left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
 if button:
-  right_column.write('ここは右カラム')
+    right_column.write('ここは右カラム')
 
 
 'Start!!'
@@ -88,9 +88,9 @@ latest_iteration = st.empty()
 bar = st.progress(0)
 
 for i in range(100):
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i+1)
-  time.sleep(0.1)
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
 
 expander1 = st.expander('お問い合わせ')
 expander1.write('問い合わせ1の回答')
@@ -98,4 +98,3 @@ expander2 = st.expander('お問い合わせ2')
 expander2.write('問い合わせ2の回答')
 expander3 = st.expander('お問い合わせ3')
 expander3.write('問い合わせ3の回答')
-
